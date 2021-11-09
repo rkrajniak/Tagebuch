2193d1ca69ad3f7c01768a2e6807e6602ba3ace8
rkrajniak <robinpatrickkrajniak@gmail.com>
2021-11-09 14:57:07 +0100

    Mapping import hinzugefügt

--- src/main/java/de/htwberlin/webtech/web/PersonRestController.java
+++ src/main/java/de/htwberlin/webtech/web/PersonRestController.java
@@ -6,7 +6,10 @@ import de.htwberlin.webtech.web.api.Person;
 import de.htwberlin.webtech.web.api.PersonManipulationRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
