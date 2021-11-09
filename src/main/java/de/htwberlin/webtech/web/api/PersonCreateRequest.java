b62b100c330034c86508cfbe32a35c356e01670b
rkrajniak <robinpatrickkrajniak@gmail.com>
2021-11-09 12:39:12 +0100

    Rest API Ressource anlegen

---
+++ src/main/java/de/htwberlin/webtech/web/api/PersonCreateRequest.java
@@ -0,0 +1,38 @@
+package de.htwberlin.webtech.web.api;
+
+public class PersonCreateRequest {
+
+    private String firstName;
+    private String lastname;
+    private boolean vaccinated;
+
+    public PersonCreateRequest(String firstName, String lastname, boolean vaccinated) {
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.vaccinated = vaccinated;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public boolean isVaccinated() {
+        return vaccinated;
+    }
+
+    public void setVaccinated(boolean vaccinated) {
+        this.vaccinated = vaccinated;
+    }
+}
